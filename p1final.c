f6d3f6622bf3e309b23846de1fb8e36410ed45cb
Oshkoshank <bandstars45@gmail.com>
2022-01-11 15:20:54 +0530

    2nd commit

---
+++ p1final.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int x,y,z;
+ printf("Enter the first number\n");
+scanf("%d",&x);
+printf("Enter the second number\n");
+scanf("%d",&y);
+z=x+y;
+printf("The addition of %d + %d is = %f\n",x,y,z);
+return 0;
+}
